e5605e11fc229053133f922e168b74aa24f40486
mpadulo <maxi.padulo@gmail.com>
2013-07-16 17:20:42 +0200

    Failing to recognize failed builds
    text key when successful: ['build', 'successful']
    text key when failed at early steps: ['failed']
    
    I think you are making a mistake not setting the error flag, when the text key has only one entry. At least for me was a problem :)
    
    I found this when using Builbot version 0.8.5

--- server/bb-arduino.php
+++ server/bb-arduino.php
@@ -3,10 +3,14 @@
 	$builders = json_decode(join('', file('http://localhost:8010/json/builders?as_text=1')));
 	foreach ($builders as $buildername => $builder) {
 		$detail = json_decode(join('', file('http://localhost:8010/json/builders/' . $buildername . '/builds/-1?as_text=1')));
-		if (count($detail->{'times'}) == 2 && $detail->{'times'}[1] != null) {
-			if (count($detail->{'text'}) > 1) {
-				if ($detail->{'text'}[1] != 'successful')
+		if (count($detail->{'times'}) == 2 )
+			if ($detail->{'times'}[1] != null) {
+				if (count($detail->{'text'}) > 1) {
+					if ($detail->{'text'}[1] != 'successful')
+						$errors = true;
+				} else {
 					$errors = true;
+				}
 			}
 		}
 	}
